230cee4db279c516833714e97a6a87ef6c125cf0
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2023-12-18 21:19:04 +0900

    Add files via upload

---
+++ src/Study02.java
@@ -0,0 +1,12 @@
+public class Study02 {
+    public static void main(String[] args) {
+
+        int a,b,c;
+        a=5;
+        b=4;
+        c=a*b;
+
+
+        System.out.printf("%d * %d = %d",a,b,c);
+    }
+}
