662aa4641ba26be1965d18ee7e1cf21c36a087f0
이재오 <dlwodh8746@naver.com>
2023-12-18 21:11:14 +0900

    자바공부

---
+++ src/Java01.java
@@ -0,0 +1,5 @@
+public class Java01 {
+    public static void main(String[] args) {
+        System.out.println("이번 달 안에 자바 무조건 마스터한다!");
+    }
+}
---
+++ src/Study01.java
@@ -0,0 +1,11 @@
+public class Study01 {
+
+    public static void main(String[] args) {
+
+        int a,b,c;
+        a=1;
+        b=2;
+        c=a+b;
+        System.out.printf("%d + %d = %d",a,b,c);
+    }
+}
