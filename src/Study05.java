5bd63233b34adfdfb29778e501caaf254ec25f07
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2023-12-21 20:56:09 +0900

    Add files via upload
    자바공부

---
+++ src/Study05.java
@@ -0,0 +1,20 @@
+public class Study05 {
+    public static void main(String[] args) {
+
+
+        String str1 = "" +
+
+                "\t\"id\"：\"winter\",\n" +
+                "\t\"name\"：\"눈송이\"\n"
+                ;
+        System.out.println(str1);
+
+
+
+        String str2 = "" +
+                "\"이름:\" \"막걸리\"\t" +
+                "\"원산지:\" \"충청도\"";
+
+        System.out.println(str2);
+    }
+}
