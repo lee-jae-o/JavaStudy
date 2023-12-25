1991e276e8eb34967e22b75f3a5ba0e7672f2215
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2023-12-25 14:27:06 +0900

    Add files via upload

---
+++ src/Study09.java
@@ -0,0 +1,25 @@
+
+
+public class Study09 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 10;
+        int result = a+b;
+
+        a++;
+        System.out.println("a: "+a);
+
+
+        System.out.println("a+b: "+result);
+
+
+        System.out.println("--------------------");
+
+        b--;
+        --b;
+        System.out.println("b: "+b);
+
+
+    }
+}
