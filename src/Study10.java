13d51b2500bde7c0841b599eeb2047de3d2e0bf1
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2023-12-26 21:05:26 +0900

    Add files via upload

---
+++ src/Study10.java
@@ -0,0 +1,17 @@
+public class Study10 {
+    public static void main(String[] args) {
+        int a = 125;
+        for (int i = 0; i < 5; i++) {
+            a++;
+            System.out.println("a: " + a);
+        }
+
+        System.out.println("----------------------");
+
+        int b = -125;
+        for (int i = 0; i < 5; i++){
+            b--;
+            System.out.println("b: " + b);
+        }
+    }
+}
