fd89567d2c260fe8aa3154587a6c44e13c394d59
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2024-02-21 09:42:56 +0900

    Add files via upload

---
+++ Study34.java
@@ -0,0 +1,13 @@
+public class Study34 {
+    public static void main(String[] args) {
+
+        int hap = 0;
+        int i;
+
+        for (i = 1; i <= 100; i++) {
+            hap += i;
+        }
+
+        System.out.println("1~100까지의 합: " + hap);
+    }
+}
