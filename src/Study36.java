35f4adda7797319d33339ae96ee9cc94445b4b52
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2024-02-26 09:01:37 +0900

    Add files via upload

---
+++ src/Study36.java
@@ -0,0 +1,15 @@
+public class Study36 {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        int i = 1;
+
+        while (i<=100) {
+            sum += i;
+            i++;
+        }
+        System.out.println("1~100까지의 합: " + sum);
+    }
+}
